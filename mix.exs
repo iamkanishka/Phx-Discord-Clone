30dee527a3bbca63fdcee4d2307cc86d0a23ff17
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-22 18:54:50 +0530

    dep: Added ueberauth dep

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule DiscordClone.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:ueberauth, "~> 0.10"},
     ]
   end
 

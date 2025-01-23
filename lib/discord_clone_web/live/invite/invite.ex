21fdc64defe2dde0fe41f6691bddc9db550583bc
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-23 18:22:39 +0530

    chore: Invite page initial commit

---
+++ lib/discord_clone_web/live/invite/invite.ex
@@ -0,0 +1,14 @@
+defmodule DiscordCloneWeb.Invite.Invite do
+  use DiscordCloneWeb, :live_view
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    """
+  end
+
+  @impl true
+  def mount(params, session, socket) do
+    {:ok, socket}
+  end
+end

005e7d20d6fc45588815d34bd0cb93871af88f43
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-23 18:22:08 +0530

    chore: Auth page initial commit

---
+++ lib/discord_clone_web/live/custom_components/auth/auth.ex
@@ -0,0 +1,15 @@
+
+defmodule DiscordCloneWeb.CustomComponents.Auth.Auth do
+  use DiscordCloneWeb, :live_component
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    """
+  end
+
+  @impl true
+  def update(assigns, socket) do
+    {:ok, socket |> assign(assigns)}
+  end
+end

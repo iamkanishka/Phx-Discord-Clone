25534728f853826557e9e1988e43d3b069d77c42
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-23 17:34:30 +0530

    chore: Added parameters

--- priv/repo/migrations/20250122132923_create_users.exs
+++ priv/repo/migrations/20250122132923_create_users.exs
@@ -3,8 +3,14 @@ defmodule DiscordClone.Repo.Migrations.CreateUsers do
 
   def change do
     create table(:users) do
+      add :email, :string, null: true
+      add :name, :string, null: true
+      add :image, :string
+      add :token, :string
 
       timestamps(type: :utc_datetime)
     end
+
+    create unique_index(:users, [:email]) # Ensure email is unique
   end
 end

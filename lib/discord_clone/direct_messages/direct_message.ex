10a6e6c7d8eebfbd2c04c6ddbdb421ee15ebc5cf
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-27 18:45:22 +0530

    feat: Added DirectMessage Schema

---
+++ lib/discord_clone/direct_messages/direct_message.ex
@@ -0,0 +1,26 @@
+defmodule DiscordClone.DirectMessages.DirectMessage do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "direct_messages" do
+    field :content, :string
+    field :file_url, :string
+    field :deleted, :boolean, default: false
+
+    belongs_to :member, YourApp.Members.Member, foreign_key: :member_id, type: :binary_id, on_replace: :delete
+    belongs_to :conversation, YourApp.Conversations.Conversation, foreign_key: :conversation_id, type: :binary_id, on_replace: :delete
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(direct_message, attrs) do
+    direct_message
+    |> cast(attrs, [:content, :file_url, :member_id, :conversation_id, :deleted])
+    |> validate_required([:content, :member_id, :conversation_id])
+    |> foreign_key_constraint(:member_id)
+    |> foreign_key_constraint(:conversation_id)
+  end
+end

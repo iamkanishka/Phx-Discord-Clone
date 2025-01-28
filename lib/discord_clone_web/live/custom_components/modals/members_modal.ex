7f5c7263fcd150f167bc7f826a3dae0319fa1e23
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-28 23:31:23 +0530

    feat: members model

--- lib/discord_clone_web/live/custom_components/modals/members_modal.ex
+++ lib/discord_clone_web/live/custom_components/modals/members_modal.ex
@@ -5,23 +5,82 @@ defmodule DiscordCloneWeb.CustomComponents.Modals.MembersModal do
   @impl true
   def render(assigns) do
     ~H"""
-    <div class="fixed inset-0 z-10 bg-black bg-opacity-50 flex justify-center items-center">
-      <div class="bg-white p-4 rounded-lg w-96">
-        <h1 class="text-2xl font-semibold">Create Channel</h1>
-
-        <form phx-submit="create_channel">
-          <input
-            type="text"
-            name="channel_name"
-            placeholder="Channel Name"
-            class="w-full border border-gray-300 rounded-lg p-2 mt-2"
-          />
-          <button type="submit" class="bg-blue-500 text-white rounded-lg p-2 mt-2 w-full">
-            Create
-          </button>
-        </form>
-      </div>
-    </div>
+    <Dialog open={isModalOpen} onOpenChange={handleClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Create Channel
+          </DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel
+                      className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70"
+                    >
+                      Channel name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                        placeholder="Enter channel name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="type"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Channel Type</FormLabel>
+                    <Select
+                      disabled={isLoading}
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger
+                          className="bg-zinc-300/50 border-0 focus:ring-0 text-black ring-offset-0 focus:ring-offset-0 capitalize outline-none"
+                        >
+                          <SelectValue placeholder="Select a channel type" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {Object.values(ChannelType).map((type) => (
+                          <SelectItem
+                            key={type}
+                            value={type}
+                            className="capitalize"
+                          >
+                            {type.toLowerCase()}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+              <Button variant="primary" disabled={isLoading}>
+                Create
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
     """
   end
 

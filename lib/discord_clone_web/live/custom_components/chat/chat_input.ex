882549cee8db51b31284138689995154a2859426
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-29 23:51:55 +0530

    chore: Updated Dialog

--- lib/discord_clone_web/live/custom_components/chat/chat_input.ex
+++ lib/discord_clone_web/live/custom_components/chat/chat_input.ex
@@ -4,6 +4,41 @@ defmodule DiscordCloneWeb.CustomComponents.Chat.ChatInput do
   @impl true
   def render(assigns) do
     ~H"""
+
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <div className="relative p-4 pb-6">
+                  <button
+                    type="button"
+                    onClick={() => onOpen("messageFile", { apiUrl, query })}
+                    className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
+                  >
+                    <Plus className="text-white dark:text-[#313338]" />
+                  </button>
+                  <Input
+                    disabled={isLoading}
+                    className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
+                    placeholder={`Message ${type === "conversation" ? name : "#" + name}`}
+                    {...field}
+                  />
+                  <div className="absolute top-7 right-8">
+                    <EmojiPicker
+                      onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
+                    />
+                  </div>
+                </div>
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
     """
   end
 

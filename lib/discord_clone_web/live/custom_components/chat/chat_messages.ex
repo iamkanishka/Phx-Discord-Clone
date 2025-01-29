f3d4aec0d0286e09d6fff583a74020aa7452d75a
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-29 23:52:36 +0530

    chore: Updated Dialog

--- lib/discord_clone_web/live/custom_components/chat/chat_messages.ex
+++ lib/discord_clone_web/live/custom_components/chat/chat_messages.ex
@@ -4,6 +4,27 @@ defmodule DiscordCloneWeb.CustomComponents.Chat.ChatMessages do
   @impl true
   def render(assigns) do
     ~H"""
+     <div className="flex flex-col-reverse mt-auto">
+        {data?.pages?.map((group, i) => (
+          <Fragment key={i}>
+            {group.items.map((message: MessageWithMemberWithProfile) => (
+              <ChatItem
+                key={message.id}
+                id={message.id}
+                currentMember={member}
+                member={message.member}
+                content={message.content}
+                fileUrl={message.fileUrl}
+                deleted={message.deleted}
+                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                isUpdated={message.updatedAt !== message.createdAt}
+                socketUrl={socketUrl}
+                socketQuery={socketQuery}
+              />
+            ))}
+          </Fragment>
+        ))}
+      </div>
     """
   end
 

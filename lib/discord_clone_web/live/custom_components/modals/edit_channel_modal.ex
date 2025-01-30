6282d47d229420bc7c8a8f14b54aee80956301b9
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-30 19:02:01 +0530

    refactor: refactoed with icons and conditions

--- lib/discord_clone_web/live/custom_components/modals/edit_channel_modal.ex
+++ lib/discord_clone_web/live/custom_components/modals/edit_channel_modal.ex
@@ -5,87 +5,55 @@ defmodule DiscordCloneWeb.CustomComponents.Modals.EditChannelModal do
   @impl true
   def render(assigns) do
     ~H"""
-      <Dialog open={isModalOpen} onOpenChange={handleClose}>
-      <DialogContent className="bg-white text-black p-0 overflow-hidden">
-        <DialogHeader className="pt-8 px-6">
-          <DialogTitle className="text-2xl text-center font-bold">
-            Edit Channel
-          </DialogTitle>
-        </DialogHeader>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <div className="space-y-8 px-6">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel
-                      className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70"
-                    >
-                      Channel name
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        disabled={isLoading}
-                        className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                        placeholder="Enter channel name"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="type"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Channel Type</FormLabel>
-                    <Select
-                      disabled={isLoading}
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                    >
-                      <FormControl>
-                        <SelectTrigger
-                          className="bg-zinc-300/50 border-0 focus:ring-0 text-black ring-offset-0 focus:ring-offset-0 capitalize outline-none"
-                        >
-                          <SelectValue placeholder="Select a channel type" />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        {Object.values(ChannelType).map((type) => (
-                          <SelectItem
-                            key={type}
-                            value={type}
-                            className="capitalize"
-                          >
-                            {type.toLowerCase()}
-                          </SelectItem>
-                        ))}
-                      </SelectContent>
-                    </Select>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+     <div class="bg-white text-black p-0 overflow-hidden">
+      <.header class="pt-8 px-6">
+     <span class="text-2xl text-center font-bold"> Edit Channel  </span>
+
+      </.header>
+
+      <.simple_form
+        for={@form}
+        id="product-form"
+        phx-target={@myself}
+        phx-change="validate"
+        phx-submit="save"
+          class="space-y-8"
+      >
+      <div class="space-y-8 px-6">
+
+        <.input field={@form[:channel_name]} type="text" label="Channel Name" />
+        <.input
+            field={@form[:channel_type]}
+            type="select"
+            label="channel Type"
+            options={["TEXT", "AUDIO", "VIDEO"]}
+            class="bg-zinc-300/50 border-0 focus:ring-0 text-black ring-offset-0 focus:ring-offset-0 capitalize outline-none"
+
+            placeholder="Select a channel type"
+            />
             </div>
-            <DialogFooter className="bg-gray-100 px-6 py-4">
-              <Button variant="primary" disabled={isLoading}>
-                Save
-              </Button>
-            </DialogFooter>
-          </form>
-        </Form>
-      </DialogContent>
-    </Dialog>
+
+        <:actions>
+        <div class="bg-gray-100 px-6 py-4">
+          <.button disabled={@isLoading} phx-disable-with="Creating...">Save</.button>
+
+          </div>
+
+
+        </:actions>
+      </.simple_form>
+    </div>
     """
   end
 
   @impl true
   def update(assigns, socket) do
-    {:ok, socket}
+    {:ok, socket|> assign(assigns)|>assign_form()}
   end
+
+  defp assign_form(socket) do
+    form = Phoenix.HTML.FormData.to_form(%{}, as: :form)
+    assign(socket, %{form: form})
+  end
+
 end

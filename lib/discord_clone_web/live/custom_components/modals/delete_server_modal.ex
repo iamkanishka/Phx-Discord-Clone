495fe2c09ad0a095607436dbbb4b74237fdbce6b
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-30 19:01:57 +0530

    refactor: refactoed with icons and conditions

--- lib/discord_clone_web/live/custom_components/modals/delete_server_modal.ex
+++ lib/discord_clone_web/live/custom_components/modals/delete_server_modal.ex
@@ -4,42 +4,51 @@ defmodule DiscordCloneWeb.CustomComponents.Modals.DeleteServerModal do
   @impl true
   def render(assigns) do
     ~H"""
-       <Dialog open={isModalOpen} onOpenChange={onClose}>
-      <DialogContent className="bg-white text-black p-0 overflow-hidden">
-        <DialogHeader className="pt-8 px-6">
-          <DialogTitle className="text-2xl text-center font-bold">
-            Delete Server
-          </DialogTitle>
-          <DialogDescription className="text-center text-zinc-500">
-            Are you sure you want to do this? <br />
-            <span className="text-indigo-500 font-semibold">{server?.name}</span> will be permanently deleted.
-          </DialogDescription>
-        </DialogHeader>
-        <DialogFooter className="bg-gray-100 px-6 py-4">
-          <div className="flex items-center justify-between w-full">
-            <Button
-              disabled={isLoading}
-              onClick={onClose}
-              variant="ghost"
-            >
-              Cancel
-            </Button>
-            <Button
-              disabled={isLoading}
-              variant="primary"
-              onClick={onClick}
-            >
-              Confirm
-            </Button>
-          </div>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+    <div class="bg-white text-black p-0 overflow-hidden">
+      <.header>
+        Delete Server
+      </.header>
+
+      <div class="text-center text-zinc-500">
+        Are you sure you want to do this? <br />
+        <span class="text-indigo-500 font-semibold">{@server?.name}</span>
+        will be permanently deleted.
+      </div>
+
+      <div class=" flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2 bg-gray-100 px-6 py-4">
+        <div class="flex items-center justify-between w-full">
+          <.button disabled={isLoading} phx-click="on_close" phx-target={@myself} class="ghost">
+            Cancel
+          </.button>
+
+          <.button
+            disabled={isLoading}
+            class="primary"
+            phc-click="on_delete_confirm"
+            phx-target={@myself}
+          >
+            Confirm
+          </.button>
+        </div>
+      </div>
+    </div>
+
+
     """
   end
 
   @impl true
   def update(assigns, socket) do
-    {:ok, socket}
+    {:ok, socket |> assign(assigns)}
+  end
+
+  @impl true
+  def handle_event("on_close", unsigned_params, socket) do
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("on_delete_confirm", unsigned_params, socket) do
+    {:noreply, socket}
   end
 end

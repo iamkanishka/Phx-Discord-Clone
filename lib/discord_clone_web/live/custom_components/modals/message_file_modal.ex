a988261b1025588236fb70a98bec7a49b328e579
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-30 19:02:22 +0530

    refactor: refactoed with icons and conditions

--- lib/discord_clone_web/live/custom_components/modals/message_file_modal.ex
+++ lib/discord_clone_web/live/custom_components/modals/message_file_modal.ex
@@ -5,46 +5,46 @@ defmodule DiscordCloneWeb.CustomComponents.Modals.MessageFileModal do
   @impl true
   def render(assigns) do
     ~H"""
-        <Dialog open={isModalOpen} onOpenChange={handleClose}>
-      <DialogContent className="bg-white text-black p-0 overflow-hidden">
-        <DialogHeader className="pt-8 px-6">
-          <DialogTitle className="text-2xl text-center font-bold">
-            Add an attachment
-          </DialogTitle>
-          <DialogDescription className="text-center text-zinc-500">
-            Send a file as a message
-          </DialogDescription>
-        </DialogHeader>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <div className="space-y-8 px-6">
-              <div className="flex items-center justify-center text-center">
-                <FormField
-                  control={form.control}
-                  name="fileUrl"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <FileUpload
-                          endpoint="messageFile"
-                          value={field.value}
-                          onChange={field.onChange}
-                        />
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-              </div>
-            </div>
-            <DialogFooter className="bg-gray-100 px-6 py-4">
-              <Button variant="primary" disabled={isLoading}>
-                Send
+
+    <div class="bg-white text-black p-0 overflow-hidden">
+      <.header class="pt-8 px-6">
+        <span  class="text-2xl text-center font-bold">Add an attachment</span>
+        <:subtitle>
+        <span class="text-center text-zinc-500">
+        Send a file as a messages</span></:subtitle>
+      </.header>
+
+      <.simple_form
+        for={@form}
+        id="product-form"
+        phx-target={@myself}
+        phx-change="validate"
+        phx-submit="save"
+        class="space-y-8"
+      >
+      <div class="space-y-8 px-6">
+              <div class="flex items-center justify-center text-center">
+              <.input
+            field={@form[:server_image]}
+            type="file"
+            label="Server Image"
+            />
+        </div>
+
+
+        <:actions>
+        <div class="bg-gray-100 px-6 py-4">
+
+          <.button phx-disable-with="Sending..."  disabled={@isLoading}>Send</.button>
+              <Button class="primary">
+                Create
               </Button>
-            </DialogFooter>
-          </form>
-        </Form>
-      </DialogContent>
-    </Dialog>
+            </div>
+        </:actions>
+      </.simple_form>
+    </div>
+
+
     """
   end
 

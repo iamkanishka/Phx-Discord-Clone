868752151170b2a2213121da9b6ce9ba3189d9c0
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-31 20:58:58 +0530

    style: Format

--- lib/discord_clone_web/live/custom_components/modals/edit_server_modal.ex
+++ lib/discord_clone_web/live/custom_components/modals/edit_server_modal.ex
@@ -1,16 +1,17 @@
-
 defmodule DiscordCloneWeb.CustomComponents.Modals.EditServerModal do
   use DiscordCloneWeb, :live_component
 
   @impl true
   def render(assigns) do
     ~H"""
-     <div class="bg-white text-black p-0 overflow-hidden">
+    <div class="bg-white text-black p-0 overflow-hidden">
       <.header class="pt-8 px-6">
-        <span  class="text-2xl text-center font-bold">Edit your server</span>
+        <span class="text-2xl text-center font-bold">Edit your server</span>
         <:subtitle>
-        <span class="text-center text-zinc-500">
-        Give your server a personality with a name and an image. You can always change it later. </span></:subtitle>
+          <span class="text-center text-zinc-500">
+            Give your server a personality with a name and an image. You can always change it later.
+          </span>
+        </:subtitle>
       </.header>
 
       <.simple_form
@@ -21,30 +22,24 @@ defmodule DiscordCloneWeb.CustomComponents.Modals.EditServerModal do
         phx-submit="save"
         class="space-y-8"
       >
-      <div class="space-y-8 px-6">
-              <div class="flex items-center justify-center text-center">
-              <.input
-            field={@form[:server_image]}
-            type="file"
-            label="Server Image"
-            />
+        <div class="space-y-8 px-6">
+          <div class="flex items-center justify-center text-center">
+            <.input field={@form[:server_image]} type="file" label="Server Image" />
+          </div>
+
+          <.input
+            field={@form[:server_name]}
+            type="text"
+            label="Server Name"
+            className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+            placeholder="Enter server name"
+          />
         </div>
 
-
-
-        <.input field={@form[:server_name]} type="text" label="Server Name"
-          className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                        placeholder="Enter server name"
-         />
-
         <:actions>
-        <div className="bg-gray-100 px-6 py-4">
-
-          <.button phx-disable-with="Creating..."  disabled={@isLoading}>Save</.button>
-              <Button variant="primary">
-                Create
-              </Button>
-            </div>
+          <div className="bg-gray-100 px-6 py-4">
+            <.button phx-disable-with="Creating..." disabled={@isLoading}>Save</.button>
+          </div>
         </:actions>
       </.simple_form>
     </div>
@@ -53,12 +48,11 @@ defmodule DiscordCloneWeb.CustomComponents.Modals.EditServerModal do
 
   @impl true
   def update(assigns, socket) do
-    {:ok, socket|> assign(assigns)|>assign_form()}
+    {:ok, socket |> assign(assigns) |> assign_form()}
   end
 
   defp assign_form(socket) do
     form = Phoenix.HTML.FormData.to_form(%{}, as: :form)
     assign(socket, %{form: form})
   end
-
 end

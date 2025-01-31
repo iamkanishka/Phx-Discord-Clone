49200a1f7de1f4edfa5e721b358fa4c9538e4092
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-31 20:55:41 +0530

    refactor: Refactored with Chatinput

--- lib/discord_clone_web/live/custom_components/chat/chat_messages.ex
+++ lib/discord_clone_web/live/custom_components/chat/chat_messages.ex
@@ -1,35 +1,45 @@
 defmodule DiscordCloneWeb.CustomComponents.Chat.ChatMessages do
   use DiscordCloneWeb, :live_component
 
+
+
+
+
+
   @impl true
   def render(assigns) do
     ~H"""
-     <div className="flex flex-col-reverse mt-auto">
-        {data?.pages?.map((group, i) => (
-          <Fragment key={i}>
-            {group.items.map((message: MessageWithMemberWithProfile) => (
-              <ChatItem
-                key={message.id}
-                id={message.id}
-                currentMember={member}
-                member={message.member}
-                content={message.content}
-                fileUrl={message.fileUrl}
-                deleted={message.deleted}
-                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
-                isUpdated={message.updatedAt !== message.createdAt}
-                socketUrl={socketUrl}
-                socketQuery={socketQuery}
-              />
-            ))}
-          </Fragment>
-        ))}
-      </div>
+    <div class="flex flex-col-reverse mt-auto">
+      <%= for {group, index} <- Enum.with_index(@data.pages || []) do %>
+        <%= for message <- group.items do %>
+          <.live_component
+            module={DiscordCloneWeb.CustomComponents.Chat.ChatInput}
+            id={message.id}
+            current_member={@member}
+            member={message.member}
+            content={message.content}
+            file_url={message.file_url}
+            deleted={message.deleted}
+            timestamp={format_datetime(message.created_at)}
+            is_updated={message.updated_at != message.created_at}
+            socket_url={@socket_url}
+            socket_query={@socket_query}
+          />
+        <% end %>
+      <% end %>
+    </div>
     """
   end
 
+
   @impl true
   def update(assigns, socket) do
-    {:ok, socket|> assign(assigns)}
+    {:ok, assign(socket, assigns)}
   end
+
+
+  defp format_datetime(datetime) do
+    Timex.format!(datetime, "%Y-%m-%d %H:%M:%S", :strftime)
+  end
+
 end

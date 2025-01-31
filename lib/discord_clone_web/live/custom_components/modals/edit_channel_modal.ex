6a5e1765bfccf6df093bb0994c93c95e30fb9454
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-31 20:58:39 +0530

    chore: Added "@" for variables in template and format

--- lib/discord_clone_web/live/custom_components/modals/edit_channel_modal.ex
+++ lib/discord_clone_web/live/custom_components/modals/edit_channel_modal.ex
@@ -1,14 +1,12 @@
-
 defmodule DiscordCloneWeb.CustomComponents.Modals.EditChannelModal do
   use DiscordCloneWeb, :live_component
 
   @impl true
   def render(assigns) do
     ~H"""
-     <div class="bg-white text-black p-0 overflow-hidden">
+    <div class="bg-white text-black p-0 overflow-hidden">
       <.header class="pt-8 px-6">
-     <span class="text-2xl text-center font-bold"> Edit Channel  </span>
-
+        <span class="text-2xl text-center font-bold"> Edit Channel </span>
       </.header>
 
       <.simple_form
@@ -17,29 +15,24 @@ defmodule DiscordCloneWeb.CustomComponents.Modals.EditChannelModal do
         phx-target={@myself}
         phx-change="validate"
         phx-submit="save"
-          class="space-y-8"
+        class="space-y-8"
       >
-      <div class="space-y-8 px-6">
-
-        <.input field={@form[:channel_name]} type="text" label="Channel Name" />
-        <.input
+        <div class="space-y-8 px-6">
+          <.input field={@form[:channel_name]} type="text" label="Channel Name" />
+          <.input
             field={@form[:channel_type]}
             type="select"
             label="channel Type"
             options={["TEXT", "AUDIO", "VIDEO"]}
             class="bg-zinc-300/50 border-0 focus:ring-0 text-black ring-offset-0 focus:ring-offset-0 capitalize outline-none"
-
             placeholder="Select a channel type"
-            />
-            </div>
+          />
+        </div>
 
         <:actions>
-        <div class="bg-gray-100 px-6 py-4">
-          <.button disabled={@isLoading} phx-disable-with="Creating...">Save</.button>
-
+          <div class="bg-gray-100 px-6 py-4">
+            <.button disabled={@isLoading} phx-disable-with="Creating...">Save</.button>
           </div>
-
-
         </:actions>
       </.simple_form>
     </div>
@@ -48,12 +41,11 @@ defmodule DiscordCloneWeb.CustomComponents.Modals.EditChannelModal do
 
   @impl true
   def update(assigns, socket) do
-    {:ok, socket|> assign(assigns)|>assign_form()}
+    {:ok, socket |> assign(assigns) |> assign_form()}
   end
 
   defp assign_form(socket) do
     form = Phoenix.HTML.FormData.to_form(%{}, as: :form)
     assign(socket, %{form: form})
   end
-
 end

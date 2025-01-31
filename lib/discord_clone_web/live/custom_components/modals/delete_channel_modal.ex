c40f4475a5d51414d3ddec48799258f99e13b822
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-31 20:57:55 +0530

    chore: Added "@" for variables in template

--- lib/discord_clone_web/live/custom_components/modals/delete_channel_modal.ex
+++ lib/discord_clone_web/live/custom_components/modals/delete_channel_modal.ex
@@ -11,41 +11,37 @@ defmodule DiscordCloneWeb.CustomComponents.Modals.DeleteChannelModal do
 
       <div class="text-center text-zinc-500">
         Are you sure you want to do this? <br />
-        <span class="text-indigo-500 font-semibold">#{channel?.name}</span>
+        <span class="text-indigo-500 font-semibold">#{@channel?.name}</span>
         will be permanently deleted.
       </div>
 
       <div class=" flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2 bg-gray-100 px-6 py-4">
         <div class="flex items-center justify-between w-full">
-          <.button disabled={isLoading} phx-click="on_close" class="ghost">
+          <.button disabled={@isLoading} phx-click="on_close" class="ghost">
             Cancel
           </.button>
 
-          <.button disabled={isLoading} class="primary" phc-click="on_delete_confirm">
+          <.button disabled={@isLoading} class="primary" phc-click="on_delete_confirm">
             Confirm
           </.button>
         </div>
       </div>
     </div>
-
     """
   end
 
   @impl true
   def update(assigns, socket) do
-    {:ok, socket|> assign(assigns) }
+    {:ok, socket |> assign(assigns)}
   end
 
   @impl true
   def handle_event("on_close", unsigned_params, socket) do
-
     {:noreply, socket}
   end
 
   @impl true
   def handle_event("on_delete_confirm", unsigned_params, socket) do
-
     {:noreply, socket}
   end
-
 end

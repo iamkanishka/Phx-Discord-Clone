7655c9af7a4904021379e4d1fa36680cfa39debb
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-31 21:01:26 +0530

    Inprogress

--- lib/discord_clone_web/live/custom_components/modals/members_modal.ex
+++ lib/discord_clone_web/live/custom_components/modals/members_modal.ex
@@ -5,44 +5,137 @@ defmodule DiscordCloneWeb.CustomComponents.Modals.MembersModal do
   @impl true
   def render(assigns) do
     ~H"""
-     <Dialog open={isModalOpen} onOpenChange={onClose}>
-      <DialogContent className="bg-white text-black overflow-hidden">
-        <DialogHeader className="pt-8 px-6">
-          <DialogTitle className="text-2xl text-center font-bold">
-            Manage Members
-          </DialogTitle>
-          <DialogDescription
-            className="text-center text-zinc-500"
-          >
-            {server?.members?.length} Members
-          </DialogDescription>
-        </DialogHeader>
-        <ScrollArea className="mt-8 max-h-[420px] pr-6">
-          {server?.members?.map((member) => (
-            <div key={member.id} className="flex items-center gap-x-2 mb-6">
-              <UserAvatar src={member.profile.imageUrl} />
-              <div className="flex flex-col gap-y-1">
-                <div className="text-xs font-semibold flex items-center gap-x-1">
+
+     <div class="bg-white text-black overflow-hidden">
+
+    <div class="bg-white text-black p-0 overflow-hidden">
+      <.header class="pt-8 px-6">
+      Manage Members
+      </.header>
+
+      <div class="text-center text-zinc-500">
+        {@server?.members?.length} Members
+      </div>
+      </div>
+        <.scroll_area class="mt-8 max-h-[420px] pr-6">
+
+
+
+
+        <%= for {member, index} <- Enum.with_index(@server?.members)do %>
+
+        <div key={member.id} class="flex items-center gap-x-2 mb-6">
+
+              <img   src={@member.profile.imageUrl} alt="Rounded avatar"  />
+                <div class="flex flex-col gap-y-1">
+                <div class="text-xs font-semibold flex items-center gap-x-1">
                   {member.profile.name}
                   {roleIconMap[member.role]}
                 </div>
-                <p className="text-xs text-zinc-500">
+                <p class="text-xs text-zinc-500">
+                  {member.profile.email}
+                </p>
+              </div>
+
+
+
+          <%!-- <li
+            phx-click="open_modal"
+            phx-target={@myself}
+            phx-value-index={index}
+            phx-click="open_modal"
+            phx-value-module={dropdown_option[:module]}
+            phx-value-id={dropdown_option[:id]}
+          >
+            <a
+              href="#"
+              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white "
+            >
+              {dropdown_option[:label]}
+              <.icon name={"hero-#{dropdown_option[:icon]}"} class="w-4 h-4" />
+            </a>
+          </li> --%>
+
+
+
+          <%= if @server.profileId != @member.profileId and @loadingId != @member.id %>
+
+
+
+          <button
+      id="dropdownDefaultButton"
+      data-dropdown-toggle="dropdown"
+      class="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
+      type="button"
+    >
+      <.icon name="hero-ellipsis-vertical" class="w-4 h-4" />
+    </button>
+
+
+          <div
+      id="dropdown"
+      class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]"
+    >
+      <ul
+        class="py-2 text-sm text-gray-700 dark:text-gray-200"
+        aria-labelledby="dropdownDefaultButton"
+      >
+
+
+      <li
+            phx-click="open_modal"
+            phx-target={@myself}
+            phx-value-index={index}
+            phx-click="open_modal"
+            phx-value-module={dropdown_option[:module]}
+            phx-value-id={dropdown_option[:id]}
+          >
+            <a
+              href="#"
+              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white "
+            >
+              <.icon name="hero-shield-exclamation" class="w-4 h-4 mr-2" />
+              <span>Role </span>
+
+            </a>
+          </li>
+
+
+      </ul>
+
+
+        <% end %>
+
+
+        <% end %>
+
+
+
+          {server?.members?.map((member) => (
+            <div key={member.id} class="flex items-center gap-x-2 mb-6">
+              <UserAvatar src={member.profile.imageUrl} />
+              <div class="flex flex-col gap-y-1">
+                <div class="text-xs font-semibold flex items-center gap-x-1">
+                  {member.profile.name}
+                  {roleIconMap[member.role]}
+                </div>
+                <p class="text-xs text-zinc-500">
                   {member.profile.email}
                 </p>
               </div>
               {server.profileId !== member.profileId && loadingId !== member.id && (
-                <div className="ml-auto">
+                <div class="ml-auto">
                   <DropdownMenu>
                     <DropdownMenuTrigger>
-                      <MoreVertical className="h-4 w-4 text-zinc-500" />
+                      <MoreVertical class="h-4 w-4 text-zinc-500" />
                     </DropdownMenuTrigger>
                     <DropdownMenuContent side="left">
                       <DropdownMenuSub>
                         <DropdownMenuSubTrigger
-                          className="flex items-center"
+                          class="flex items-center"
                         >
                           <ShieldQuestion
-                            className="w-4 h-4 mr-2"
+                            class="w-4 h-4 mr-2"
                           />
                           <span>Role</span>
                         </DropdownMenuSubTrigger>
@@ -51,22 +144,22 @@ defmodule DiscordCloneWeb.CustomComponents.Modals.MembersModal do
                             <DropdownMenuItem
                               onClick={() => onRoleChange(member.id, "GUEST")}
                             >
-                              <Shield className="h-4 w-4 mr-2" />
+                              <Shield class="h-4 w-4 mr-2" />
                               Guest
                               {member.role === "GUEST" && (
                                 <Check
-                                  className="h-4 w-4 ml-auto"
+                                  class="h-4 w-4 ml-auto"
                                 />
                               )}
                             </DropdownMenuItem>
                             <DropdownMenuItem
                               onClick={() => onRoleChange(member.id, "MODERATOR")}
                             >
-                              <ShieldCheck className="h-4 w-4 mr-2" />
+                              <ShieldCheck class="h-4 w-4 mr-2" />
                               Moderator
                               {member.role === "MODERATOR" && (
                                 <Check
-                                  className="h-4 w-4 ml-auto"
+                                  class="h-4 w-4 ml-auto"
                                 />
                               )}
                             </DropdownMenuItem>
@@ -77,7 +170,7 @@ defmodule DiscordCloneWeb.CustomComponents.Modals.MembersModal do
                       <DropdownMenuItem
                         onClick={() => onKick(member.id)}
                       >
-                        <Gavel className="h-4 w-4 mr-2" />
+                        <Gavel class="h-4 w-4 mr-2" />
                         Kick
                       </DropdownMenuItem>
                     </DropdownMenuContent>
@@ -86,12 +179,12 @@ defmodule DiscordCloneWeb.CustomComponents.Modals.MembersModal do
               )}
               {loadingId === member.id && (
                 <Loader2
-                  className="animate-spin text-zinc-500 ml-auto w-4 h-4"
+                  class="animate-spin text-zinc-500 ml-auto w-4 h-4"
                 />
               )}
             </div>
           ))}
-        </ScrollArea>
+
       </DialogContent>
     </Dialog>
     """

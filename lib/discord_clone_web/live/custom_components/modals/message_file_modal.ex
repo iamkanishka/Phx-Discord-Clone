40eb6e07221ff464dead284b77f2fff5c3913eaa
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-25 21:42:27 +0530

    chore: Resolved Minor bugs

--- lib/discord_clone_web/live/custom_components/modals/message_file_modal.ex
+++ lib/discord_clone_web/live/custom_components/modals/message_file_modal.ex
@@ -1,17 +1,17 @@
-
 defmodule DiscordCloneWeb.CustomComponents.Modals.MessageFileModal do
   use DiscordCloneWeb, :live_component
 
   @impl true
   def render(assigns) do
     ~H"""
-
     <div class="bg-white text-black p-0 overflow-hidden">
       <.header class="pt-8 px-6">
-        <span  class="text-2xl text-center font-bold">Add an attachment</span>
+        <span class="text-2xl text-center font-bold">Add an attachment</span>
         <:subtitle>
-        <span class="text-center text-zinc-500">
-        Send a file as a messages</span></:subtitle>
+          <span class="text-center text-zinc-500">
+            Send a file as a messages
+          </span>
+        </:subtitle>
       </.header>
 
       <.simple_form
@@ -22,29 +22,19 @@ defmodule DiscordCloneWeb.CustomComponents.Modals.MessageFileModal do
         phx-submit="save"
         class="space-y-8"
       >
-      <div class="space-y-8 px-6">
-              <div class="flex items-center justify-center text-center">
-              <.input
-            field={@form[:server_image]}
-            type="file"
-            label="Server Image"
-            />
+        <div class="space-y-8 px-6">
+          <div class="flex items-center justify-center text-center">
+            <.input field={@form[:server_image]} type="file" label="Server Image" />
+          </div>
         </div>
 
-
         <:actions>
-        <div class="bg-gray-100 px-6 py-4">
-
-          <.button phx-disable-with="Sending..."  disabled={@isLoading}>Send</.button>
-              <Button class="primary">
-                Create
-              </Button>
-            </div>
+          <.button class="bg-gray-100 px-6 py-4" phx-disable-with="Sending..." disabled={@isLoading}>
+            Send
+          </.button>
         </:actions>
       </.simple_form>
     </div>
-
-
     """
   end
 

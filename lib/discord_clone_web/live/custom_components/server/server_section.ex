270de0e399354cc1ebe0c9f4ac27a9dc7260c433
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-25 21:42:47 +0530

    chore: Resolved Minor bugs

--- lib/discord_clone_web/live/custom_components/server/server_section.ex
+++ lib/discord_clone_web/live/custom_components/server/server_section.ex
@@ -6,34 +6,33 @@ defmodule DiscordCloneWeb.CustomComponents.Server.ServerSection do
     ~H"""
     <div class="flex items-center justify-between py-2">
       <p class="text-xs uppercase font-semibold text-zinc-500 dark:text-zinc-400">
-        {label}
+        {@label}
       </p>
-      <%= if role != "GUEST" and @sectionType == "channels" %>
 
-        <ActionTooltip label="Create Channel" side="top">
-          <button
-          phx-click="create_channel" phx-target={@myself}
-            class="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
-          >
-            <Plus class="h-4 w-4" />
-      <.icon name="hero-plus" class="w-4 h-4" />
+      <%= if @role != "GUEST" and @sectionType == "channels" do %>
+        <%!-- <ActionTooltip label="Create Channel" side="top"> --%>
+        <.button
+          phx-click="create_channel"
+          phx-target={@myself}
+          class="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+        >
+          <.icon name="hero-plus" class="w-4 h-4" />
+        </.button>
+         <%!-- </ActionTooltip> --%>
+      <% end %>
 
-
-          </button>
-        </ActionTooltip>
-     <% end %>
-     <%= if role != "ADMIN" and @sectionType == "members" %>
-      <%!-- <ActionTooltip label="Manage Members" side="top"> --%>
-          <button phx-click="members" phx-target={@myself} class="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition">
-
-      <.icon name="hero-cog-6-tooth" class="w-4 h-4" />
-
-    </button>
-        <%!-- </ActionTooltip> --%>
-        <% end %>
-
+      <%= if @role != "ADMIN" and @sectionType == "members" do %>
+        <%!-- <ActionTooltip label="Manage Members" side="top"> --%>
+        <button
+          phx-click="members"
+          phx-target={@myself}
+          class="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+        >
+          <.icon name="hero-cog-6-tooth" class="w-4 h-4" />
+        </button>
+         <%!-- </ActionTooltip> --%>
+      <% end %>
     </div>
-
     """
   end
 

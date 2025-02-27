f7f1b49bf3db3c12869a09f4206aa5f9801b5308
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-27 21:30:35 +0530

    feat: NavigationSidebar template

--- lib/discord_clone_web/live/custom_components/navigation/navigation_sidebar.ex
+++ lib/discord_clone_web/live/custom_components/navigation/navigation_sidebar.ex
@@ -4,6 +4,21 @@ defmodule DiscordCloneWeb.CustomComponents.Navigation.NavigationSidebar do
   @impl true
   def render(assigns) do
     ~H"""
+    <.tooltip side="right" align="center" label={@name}>
+      <button phx-click="server_click" phx-value-id={@id} class="group relative flex items-center">
+        <div class={[
+          "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
+          @params["serverId"] != @id && "group-hover:h-[20px]",
+          @params["serverId"] == @id && "h-[36px]" || "h-[8px]"
+        ]} />
+        <div class={[
+          "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
+          @params["serverId"] == @id && "bg-primary/10 text-primary rounded-[16px]"
+        ]}>
+          <img src={@image_url} alt="Channel" class="object-cover w-full h-full" />
+        </div>
+      </button>
+    </.tooltip>
     """
   end
 

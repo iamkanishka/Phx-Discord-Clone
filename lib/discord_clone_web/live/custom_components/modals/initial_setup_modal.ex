d895ca4b96c96c4370277475c2abb4dec735f5f8
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-01 22:38:38 +0530

    chore: Added profie image upload component

--- lib/discord_clone_web/live/custom_components/modals/initial_setup_modal.ex
+++ lib/discord_clone_web/live/custom_components/modals/initial_setup_modal.ex
@@ -5,12 +5,13 @@ defmodule DiscordCloneWeb.CustomComponents.Modals.InitialSetupModal do
   def render(assigns) do
     ~H"""
     <div class="bg-white text-black p-0 overflow-hidden">
-      <.header class="pt-8 px-6">
-        <span class="text-2xl text-center font-bold">Customize your server</span>
+      <.header>
+        <div class="text-2xl text-center font-bold">Customize your server</div>
+
         <:subtitle>
-          <span class="text-center text-zinc-500">
+          <div class="text-center text-zinc-500">
             Give your server a personality with a name and an image. You can always change it later.
-          </span>
+          </div>
         </:subtitle>
       </.header>
 
@@ -23,22 +24,22 @@ defmodule DiscordCloneWeb.CustomComponents.Modals.InitialSetupModal do
         class="space-y-8"
       >
         <div class="space-y-8 px-6">
-          <div class="flex items-center justify-center text-center">
-            <.input field={@form[:server_image]} type="file" label="Server Image" />
-          </div>
-
+          <.live_component
+            module={DiscordCloneWeb.CustomComponents.Shared.ProfileImageUpload}
+            id={:profile_image_upload}
+          />
           <.input
             field={@form[:server_name]}
             type="text"
             label="Server Name"
-            className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+            class="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
             placeholder="Enter server name"
           />
         </div>
 
         <:actions>
-          <div className="bg-gray-100 px-6 py-4">
-            <.button phx-disable-with="Creating..." disabled={@isLoading}>Create</.button>
+          <div class=" w-full text-center">
+            <.button phx-disable-with="Creating..." disabled={@is_loading}>Create</.button>
           </div>
         </:actions>
       </.simple_form>
@@ -48,11 +49,19 @@ defmodule DiscordCloneWeb.CustomComponents.Modals.InitialSetupModal do
 
   @impl true
   def update(assigns, socket) do
-    {:ok, socket |> assign(assigns) |> assign_form()}
+    {:ok,
+     socket
+     |> assign(assigns)
+     |> assign(:is_loading, false)
+     |> assign_form()}
   end
 
   defp assign_form(socket) do
     form = Phoenix.HTML.FormData.to_form(%{}, as: :form)
     assign(socket, %{form: form})
+  end
+
+  def handle_event("save", unsigned_params, socket) do
+    {:noreply, socket}
   end
 end

3abde4de8fc2b7a0541640198dfd688db33c7fb7
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-01 22:40:45 +0530

    chore: Added modal to Invite modal

--- lib/discord_clone_web/live/invite/invite.ex
+++ lib/discord_clone_web/live/invite/invite.ex
@@ -4,15 +4,17 @@ defmodule DiscordCloneWeb.Invite.Invite do
   @impl true
   def render(assigns) do
     ~H"""
+    <.modal   id="invite-modal" show on_cancel={JS.patch(~p"/")}>
     <.live_component
       module={DiscordCloneWeb.CustomComponents.Modals.InviteModal}
       id={:invite_invite_modal}
     />
+    </.modal>
     """
   end
 
   @impl true
-  def mount(_params, _session, socket) do
+  def mount(params, session, socket) do
     {:ok, socket}
   end
 end

1ad8e2d96e289d1b9a886629157ded248d3c0210
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-02 17:50:30 +0530

    chore: Removed foreign key constraints and indexes from conversations table migration

--- priv/repo/migrations/20250127130621_create_conversations.exs
+++ priv/repo/migrations/20250127130621_create_conversations.exs
@@ -2,16 +2,8 @@ defmodule DiscordClone.Repo.Migrations.CreateConversations do
   use Ecto.Migration
 
   def change do
-    create table(:conversations, primary_key: false) do
-      add :id, :binary_id, primary_key: true
-      add :member_one_id, references(:members, type: :binary_id, on_delete: :delete_all), null: false
-      add :member_two_id, references(:members, type: :binary_id, on_delete: :delete_all), null: false
-
+    create table(:conversations) do
       timestamps(type: :utc_datetime)
     end
-
-    create index(:conversations, [:member_two_id])
-
-    create unique_index(:conversations, [:member_one_id, :member_two_id], name: :unique_member_pair)
   end
 end

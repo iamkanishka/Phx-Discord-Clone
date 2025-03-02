a494d564dc3f60529631a67fb6b8f7786b6d34df
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-02 17:48:46 +0530

    refactor: Refactor migration to use timestamps instead of explicit created_at and updated_at fields.

--- priv/repo/migrations/20250127122255_create_members.exs
+++ priv/repo/migrations/20250127122255_create_members.exs
@@ -2,20 +2,8 @@ defmodule DiscordClone.Repo.Migrations.CreateMembers do
   use Ecto.Migration
 
   def change do
-    create table(:members, primary_key: false) do
-      add :id, :binary_id, primary_key: true
-      add :role, :string, null: false, default: "GUEST"
-
-      add :profile_id, references(:profiles, type: :binary_id, on_delete: :delete_all), null: false
-      add :server_id, references(:servers, type: :binary_id, on_delete: :delete_all), null: false
-
-      add :created_at, :utc_datetime, default: fragment("now()"), null: false
-      add :updated_at, :utc_datetime, default: fragment("now()"), null: false
-
+    create table(:members) do
       timestamps(type: :utc_datetime)
     end
-
-    create index(:members, [:profile_id])
-    create index(:members, [:server_id])
   end
 end

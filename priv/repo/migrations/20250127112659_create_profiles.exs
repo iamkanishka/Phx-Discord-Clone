3f183ccab4f2dc9c194fb87e0eeb9d1fe3c5d2ed
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-02 17:48:10 +0530

    refactor: Refactor migration to remove primary key from profiles table and remove created_at and updated_at fields.

--- priv/repo/migrations/20250127112659_create_profiles.exs
+++ priv/repo/migrations/20250127112659_create_profiles.exs
@@ -2,19 +2,12 @@ defmodule DiscordClone.Repo.Migrations.CreateProfiles do
   use Ecto.Migration
 
   def change do
-    create table(:profiles, primary_key: false) do
-      add :id, :binary_id, primary_key: true
-      add :user_id, :string, null: false
+    create table(:profiles) do
       add :name, :string, null: false
       add :image_url, :text
       add :email, :text, null: false
 
-      add :created_at, :utc_datetime, default: fragment("now()"), null: false
-      add :updated_at, :utc_datetime, default: fragment("now()"), null: false
-
       timestamps(type: :utc_datetime)
     end
-
-    create unique_index(:profiles, [:user_id])
   end
 end

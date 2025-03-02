9ad7affdd1ff770ae06997fb05c62cbaeeecde58
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-02 17:47:04 +0530

    feat: Added Server Schema

---
+++ lib/discord_clone/servers/server.ex
@@ -0,0 +1,24 @@
+defmodule DiscordClone.Servers.Server do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "servers" do
+    field :name, :string
+    field :image_url, :string
+    field :invite_code, :string
+
+    belongs_to :profile, DiscordClone.Profiles.Profile
+    has_many :members, DiscordClone.Members.Member
+    has_many :channels, DiscordClone.Channels.Channel
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(server, attrs) do
+    server
+    |> cast(attrs, [:name, :image_url, :invite_code, :profile_id])
+    |> validate_required([:name, :image_url, :invite_code, :profile_id])
+    |> unique_constraint(:invite_code)
+  end
+end

ed7f9afaae679fcf7099f0e1d387e6055b7f103d
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-02 17:51:02 +0530

    refactor: Refactor direct_messages table migration to remove primary_key: false and redundant index creation`

--- priv/repo/migrations/20250127131339_create_direct_messages.exs
+++ priv/repo/migrations/20250127131339_create_direct_messages.exs
@@ -2,19 +2,14 @@ defmodule DiscordClone.Repo.Migrations.CreateDirectMessages do
   use Ecto.Migration
 
   def change do
-    create table(:direct_messages, primary_key: false) do
-      add :id, :binary_id, primary_key: true
+    create table(:direct_messages) do
+
       add :content, :text, null: false
       add :file_url, :text
       add :deleted, :boolean, default: false
 
-      add :member_id, references(:members, type: :binary_id, on_delete: :delete_all), null: false
-      add :conversation_id, references(:conversations, type: :binary_id, on_delete: :delete_all), null: false
-
       timestamps(type: :utc_datetime)
     end
 
-    create index(:direct_messages, [:member_id])
-    create index(:direct_messages, [:conversation_id])
   end
 end

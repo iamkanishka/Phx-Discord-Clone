4175ff69b8039029784f11385293375b78a1d9a0
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-02 17:49:59 +0530

    refactor: Refactor migration to remove redundant  and simplify table creation

--- priv/repo/migrations/20250127125322_create_messages.exs
+++ priv/repo/migrations/20250127125322_create_messages.exs
@@ -1,22 +1,15 @@
 defmodule DiscordClone.Repo.Migrations.CreateMessages do
   use Ecto.Migration
 
-   use Ecto.Migration
+  use Ecto.Migration
 
   def change do
-    create table(:messages, primary_key: false) do
-      add :id, :binary_id, primary_key: true
+    create table(:messages) do
       add :content, :text, null: false
       add :file_url, :text
       add :deleted, :boolean, default: false, null: false
 
-      add :member_id, references(:members, type: :binary_id, on_delete: :delete_all), null: false
-      add :channel_id, references(:channels, type: :binary_id, on_delete: :delete_all), null: false
-
-      timestamps(type: :utc_datetime)
+      timestamps()
     end
-
-    create index(:messages, [:channel_id])
-    create index(:messages, [:member_id])
   end
-  end
+end

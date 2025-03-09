2792225346907ff6d3ca0f04d7f579dc14972fc9
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-09 21:12:59 +0530

    feat: Added create_profile

--- lib/discord_clone/profiles/profiles.ex
+++ lib/discord_clone/profiles/profiles.ex
@@ -1,4 +1,11 @@
 defmodule DiscordClone.Profiles.Profiles do
   import Ecto.Query, warn: false
+  alias DiscordClone.Repo
+  alias DiscordClone.Profiles.Profile
 
+  def create_profile(user_id) do
+    %Profile{}
+    |> Profile.changeset(%{user_id: user_id})
+    |> Repo.insert()
+  end
 end

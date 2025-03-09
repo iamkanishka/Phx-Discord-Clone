886c1eb883e0ece57bc473a1d229c4bb5b30beac
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-09 21:12:44 +0530

    chore: Removed some fields and  type: :utc_datetime

--- lib/discord_clone/profiles/profile.ex
+++ lib/discord_clone/profiles/profile.ex
@@ -2,27 +2,22 @@ defmodule DiscordClone.Profiles.Profile do
   use Ecto.Schema
   import Ecto.Changeset
 
-
   schema "profiles" do
-    field :name, :string
-    field :image_url, :string
-    field :email, :string
-
-    belongs_to :user_id, DiscordClone.Accounts.User
+    belongs_to :user, DiscordClone.Accounts.User
 
     has_many :servers, DiscordClone.Servers.Server
     has_many :members, DiscordClone.Members.Member
     has_many :channels, DiscordClone.Channels.Channel
 
-    timestamps(type: :utc_datetime)
+    timestamps()
   end
 
   @doc false
   def changeset(profile, attrs) do
     profile
-    |> cast(attrs, [:user_id, :name, :image_url, :email])
-    |> validate_required([:user_id, :name, :email])
-    |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/)
+    |> cast(attrs, [:user_id])
+    |> validate_required([:user_id])
     |> unique_constraint(:user_id)
+    |> assoc_constraint(:user_id)
   end
 end

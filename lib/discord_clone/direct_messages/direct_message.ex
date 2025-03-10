ce7e2054b27b5afcaba041fd4eb28b79cb853c2a
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-10 21:12:28 +0530

    chore: Added primary key and foreign key type to DirectMessage schema.

--- lib/discord_clone/direct_messages/direct_message.ex
+++ lib/discord_clone/direct_messages/direct_message.ex
@@ -2,6 +2,9 @@ defmodule DiscordClone.DirectMessages.DirectMessage do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @derive {Phoenix.Param, key: :id}
+  @foreign_key_type :binary_id
   schema "direct_messages" do
     field :content, :string
     field :file_url, :string

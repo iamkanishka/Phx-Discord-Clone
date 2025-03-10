edce7bfec734c8cbe982e6a6ec466d3298a10f22
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-10 21:09:58 +0530

    chore: Add primary key to servers table in migration and Modify servers table to use binary_id as primary key in migration.

--- priv/repo/migrations/20250127112659_create_profiles.exs
+++ priv/repo/migrations/20250127112659_create_profiles.exs
@@ -2,10 +2,9 @@ defmodule DiscordClone.Repo.Migrations.CreateProfiles do
   use Ecto.Migration
 
   def change do
-    create table(:profiles) do
-      add :name, :string, null: false
-      add :image_url, :text
-      add :email, :text, null: false
+    create table(:profiles, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :user_id, references(:users, type: :binary_id, on_delete: :delete_all), null: false
 
       timestamps()
     end

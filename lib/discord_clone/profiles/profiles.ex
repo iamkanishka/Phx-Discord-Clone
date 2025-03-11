663e426fc399b3eb149dcd1ef5656ba84188c68a
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-11 21:40:08 +0530

    feat: added initail profile

--- lib/discord_clone/profiles/profiles.ex
+++ lib/discord_clone/profiles/profiles.ex
@@ -3,9 +3,21 @@ defmodule DiscordClone.Profiles.Profiles do
   alias DiscordClone.Repo
   alias DiscordClone.Profiles.Profile
 
-  def create_profile(user_id) do
-    %Profile{}
-    |> Profile.changeset(%{user_id: user_id})
-    |> Repo.insert()
+  def initial_profile(user_id) do
+    case Repo.get_by(Profile, user_id: user_id) do
+      nil ->
+        create_profile(user_id)
+
+      profile ->
+        {:ok, profile}
+    end
   end
+
+
+
+  # def create_profile(user_id) do
+  #   %Profile{}
+  #   |> Profile.changeset(%{user_id: user_id})
+  #   |> Repo.insert()
+  # end
 end

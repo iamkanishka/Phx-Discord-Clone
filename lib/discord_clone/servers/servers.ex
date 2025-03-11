34b6880e9ebd1348054f7b0ab52de6194b02313d
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-11 21:40:44 +0530

    feat: added find_and_redirect_to_server fn

--- lib/discord_clone/servers/servers.ex
+++ lib/discord_clone/servers/servers.ex
@@ -1,9 +1,11 @@
 defmodule DiscordClone.Servers.Servers do
   import Ecto.Query, warn: false
+  alias DiscordClone.Profiles.Profiles
   alias DiscordClone.Repo
   alias DiscordClone.Servers.Server
   alias DiscordClone.Channels.Channel
   alias DiscordClone.Members.Member
+
 
   def create_server(profile_id, image_url, name) do
     Repo.transaction(fn ->
@@ -31,11 +33,28 @@ defmodule DiscordClone.Servers.Servers do
         |> Member.changeset(%{
           server_id: server.id,
           profile_id: profile_id,
-          role: "admin"
+          role: "ADMIN"
         })
         |> Repo.insert()
 
       server
     end)
   end
+
+
+  def find_and_redirect_to_server(user_id) do
+    with {:ok, profile} <- Profiles.initial_profile(user_id),
+         server <- get_server_by_profile(profile.id) do
+      case server do
+        nil -> {:ok, :no_server_found}
+        %Server{id: server_id} -> {:redirect, "/servers/#{server_id}"}
+      end
+    else
+      {:error, :unauthenticated} -> {:redirect, "/auth/sign_in"}
+      {:error, changeset} -> {:error, changeset}
+    end
+  end
+
+
+
 end

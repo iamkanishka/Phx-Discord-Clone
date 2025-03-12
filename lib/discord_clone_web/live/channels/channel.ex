a0761fef0056f5f4d61afc1fb6d09ef709190f51
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-12 21:50:55 +0530

    feat: Added ChatHeader

--- lib/discord_clone_web/live/channels/channel.ex
+++ lib/discord_clone_web/live/channels/channel.ex
@@ -1,62 +1,29 @@
 defmodule DiscordCloneWeb.Channels.Channel do
   use DiscordCloneWeb, :live_view
 
+  # alias MyApp.Chat
+  # alias MyAppWeb.Components.{ChatHeader, ChatMessages, ChatInput, MediaRoom}
+
   @impl true
   def render(assigns) do
     ~H"""
-      <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
-      <ChatHeader
-        name={channel.name}
-        serverId={channel.serverId}
-        type="channel"
-      />
-      {channel.type === ChannelType.TEXT && (
-        <>
-          <ChatMessages
-            member={member}
-            name={channel.name}
-            chatId={channel.id}
-            type="channel"
-            apiUrl="/api/messages"
-            socketUrl="/api/socket/messages"
-            socketQuery={{
-              channelId: channel.id,
-              serverId: channel.serverId,
-            }}
-            paramKey="channelId"
-            paramValue={channel.id}
-          />
-          <ChatInput
-            name={channel.name}
-            type="channel"
-            apiUrl="/api/socket/messages"
-            query={{
-              channelId: channel.id,
-              serverId: channel.serverId,
-            }}
-          />
-        </>
-      )}
-      {channel.type === ChannelType.AUDIO && (
-        <MediaRoom
-          chatId={channel.id}
-          video={false}
-          audio={true}
-        />
-      )}
-      {channel.type === ChannelType.VIDEO && (
-        <MediaRoom
-          chatId={channel.id}
-          video={true}
-          audio={true}
-        />
-      )}
+    <div class="bg-white dark:bg-[#313338] flex flex-col h-full">
+    <ChatHeader name={@channel.name} server_id={@channel.server_id} type="channel" />
     </div>
     """
   end
 
-  @impl true
-  def mount(params, session, socket) do
-    {:ok, socket}
+
+
+  def mount(%{"channel_id" => channel_id}, _session, socket) do
+    # channel = Chat.get_channel(channel_id)
+
+    # socket =
+    #   socket
+    #   |> assign(:channel, channel)
+    #   |> assign(:member, get_member(socket))
+
+    # {:ok, socket}
   end
+
 end

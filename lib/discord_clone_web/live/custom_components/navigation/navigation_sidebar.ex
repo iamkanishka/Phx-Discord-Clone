46354bbb9a1b06638532f1b51f3c6d0b69630cf0
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-12 21:43:46 +0530

    refactor: Refactored template and attributes

--- lib/discord_clone_web/live/custom_components/navigation/navigation_sidebar.ex
+++ lib/discord_clone_web/live/custom_components/navigation/navigation_sidebar.ex
@@ -1,31 +1,66 @@
 defmodule DiscordCloneWeb.CustomComponents.Navigation.NavigationSidebar do
+  alias DiscordClone.Servers.Servers
   use DiscordCloneWeb, :live_component
 
   @impl true
   def render(assigns) do
     ~H"""
-    <div>
-    <.tooltip side="right" align="center" label={@name}>
-      <button phx-click="server_click" phx-value-id={@id} class="group relative flex items-center">
-        <div class={[
-          "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
-          @params["serverId"] != @id && "group-hover:h-[20px]",
-          @params["serverId"] == @id && "h-[36px]" || "h-[8px]"
-        ]} />
-        <div class={[
-          "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
-          @params["serverId"] == @id && "bg-primary/10 text-primary rounded-[16px]"
-        ]}>
-          <img src={@image_url} alt="Channel" class="object-cover w-full h-full" />
+    <div class="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] bg-[#E3E5E8] py-3">
+      <.live_component
+        module={DiscordCloneWeb.CustomComponents.Navigation.NavigationAction}
+        id={:navigation_action}
+      />
+      <div class="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"></div>
+
+      <.scroll_area class="flex-1 w-full">
+        <div class="flex-1 w-full overflow-auto">
+          <%= for server <- @servers do %>
+            <div class="mb-4">
+              <.live_component
+                module={DiscordCloneWeb.CustomComponents.Navigation.NavigationItem}
+                id={"server-#{server.id}"}
+                name={server.name}
+                server_id={server.id}
+                image_url={server.image_url}
+                current_server_id={@server_id}
+              />
+            </div>
+          <% end %>
         </div>
-      </button>
-    </.tooltip>
+      </.scroll_area>
+
+          <!-- Dropdown menu   -->
+
+
+
     </div>
     """
   end
 
   @impl true
   def update(assigns, socket) do
-    {:ok, socket}
+    # IO.inspect(Servers.find_servers(assigns.user_id))
+    IO.inspect(assigns)
+
+
+    socket =
+      case Servers.find_servers(assigns.user_id) do
+        {:ok, servers} ->
+          assign(socket, :servers, servers)
+
+        # {:redirect, path} ->
+        #   push_navigate(socket, to: path, replace: true)
+
+        {:ok, :no_servers_found} ->
+          IO.puts("No server found, show server creation UI")
+          socket
+
+        {:error, changeset} ->
+          IO.inspect(changeset, label: "Error creating profile")
+          socket
+      end
+
+    {:ok, socket
+    |> assign(assigns)}
   end
 end

ddb1cb283ffb562e227f0951192becf5e9121fbd
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-12 21:46:00 +0530

    chore: Added Layout

--- lib/discord_clone_web/live/servers/server.ex
+++ lib/discord_clone_web/live/servers/server.ex
@@ -4,15 +4,27 @@ defmodule DiscordCloneWeb.Servers.Server do
   @impl true
   def render(assigns) do
     ~H"""
-    <.live_component
-      module={DiscordCloneWeb.CustomComponents.Navigation.NavigationSidebar}
-      id={:server_side_bar}
-    />
+    <div class="h-full">
+      <div class="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+        <.live_component
+          module={DiscordCloneWeb.CustomComponents.Navigation.NavigationSidebar}
+          id={:server_side_bar}
+          user_id={@user_id}
+          user_image={@user_image}
+          server_id={@server_id}
+        />
+      </div>
+
+      <main class="md:pl-[72px] h-full"></main>
+    </div>
     """
   end
 
   @impl true
-  def update(assigns, socket) do
-    {:ok, socket}
+  def mount(params, session, socket) do
+    {:ok,
+     socket }
   end
+
+
 end

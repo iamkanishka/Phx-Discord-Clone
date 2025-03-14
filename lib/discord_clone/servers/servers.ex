f14fef8b0623352a1af72f7a07c7b3bc782a252e
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-14 21:48:57 +0530

    feat: Added get_server_data fn

--- lib/discord_clone/servers/servers.ex
+++ lib/discord_clone/servers/servers.ex
@@ -65,16 +65,17 @@ defmodule DiscordClone.Servers.Servers do
   def find_servers(user_id) do
     with {:ok, profile} <- Profiles.initial_profile(user_id),
          servers <- get_servers_by_profile(profile.id) do
-          if Enum.empty?(servers) do
-            {:ok, :no_server_found}
-          else
-            {:ok, servers}
-          end
+      if Enum.empty?(servers) do
+        {:ok, :no_server_found}
+      else
+        {:ok, servers}
+      end
     else
       {:error, :unauthenticated} -> {:redirect, "/auth/sign_in"}
       {:error, changeset} -> {:error, changeset}
     end
   end
+
   defp get_servers_by_profile(profile_id) do
     Repo.all(
       from s in Server,
@@ -83,4 +84,92 @@ defmodule DiscordClone.Servers.Servers do
     )
   end
 
+
+
+  def get_server_data(server_id, profile_id) do
+    server =
+      Repo.get(Server, server_id)
+      |> Repo.preload([
+        channels: from(c in Channel, order_by: [asc: c.inserted_at]),
+        members:
+          from(m in Member,
+            order_by: [asc: m.role],
+            preload: [:profile]
+          )
+      ])
+
+    if server do
+      text_channels = Enum.filter(server.channels, &(&1.type == :TEXT))
+      audio_channels = Enum.filter(server.channels, &(&1.type == :AUDIO))
+      video_channels = Enum.filter(server.channels, &(&1.type == :VIDEO))
+
+      members = Enum.filter(server.members, &(&1.profile_id != profile_id))
+
+      role =
+        server.members
+        |> Enum.find(&(&1.profile_id == profile_id))
+        |> case do
+          nil -> nil
+          member -> member.role
+        end
+
+      %{
+        server: server,
+        text_channels: text_channels,
+        audio_channels: audio_channels,
+        video_channels: video_channels,
+        members: members,
+        role: role
+      }
+    else
+      {:error, :not_found}
+    end
+  end
+
+  # def get_server_data(server_id, profile_id) do
+  #   query =
+  #     from s in Server,
+  #       where: s.id == ^server_id,
+  #       preload: [
+  #         :profile,
+  #         members:
+  #           ^from(m in Member,
+  #             where: m.profile_id != ^profile_id,
+  #             order_by: [asc: m.role],
+  #             preload: [:profile]
+  #           ),
+  #         channels:
+  #           ^from(c in Channel,
+  #             where: c.type in [:TEXT, :AUDIO, :VIDEO],
+  #             order_by: [asc: c.inserted_at]
+  #           )
+  #       ]
+
+  #   case Repo.one(query) do
+  #     nil ->
+  #       {:error, :not_found}
+
+  #     server ->
+  #       # Extract role in a single query
+  #       role_query =
+  #         from m in Member,
+  #           where: m.server_id == ^server_id and m.profile_id == ^profile_id,
+  #           select: m.role
+
+  #       role = Repo.one(role_query)
+
+  #       # Group channels by type using Enum.group_by
+  #       channel_groups = Enum.group_by(server.channels, & &1.type)
+
+  #       %{
+  #         server: server
+  #         # text_channels: Map.get(channel_groups, :TEXT, []),
+  #         # audio_channels: Map.get(channel_groups, :AUDIO, []),
+  #         # video_channels: Map.get(channel_groups, :VIDEO, []),
+
+  #         # members: server.members,
+  #         # role: role
+  #       }
+  #   end
+  # end
 end

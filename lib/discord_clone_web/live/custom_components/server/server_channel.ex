b1cc7b6fe7aaf53dffb1f15aa0027e269575f767
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-16 21:29:42 +0530

    fix: updated server channel button .

--- lib/discord_clone_web/live/custom_components/server/server_channel.ex
+++ lib/discord_clone_web/live/custom_components/server/server_channel.ex
@@ -7,53 +7,56 @@ defmodule DiscordCloneWeb.CustomComponents.Server.ServerChannel do
     <button
       phx-click="server_click"
       phx-target={@myself}
-      class={"group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1" <> if  @params?.channelId === @channel.id, do: "bg-zinc-700/20 dark:bg-zinc-700", else: ""}
+      class={"group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1" <> if  @channel.id != @channel.id, do: "bg-zinc-700/20 dark:bg-zinc-700", else: ""}
     >
       <.icon name="hero-hashtag" class="flex-shrink-0 w-5 h-5 text-zinc-500 dark:text-zinc-400" />
       <p class={  "line-clamp-1 font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition"
-          <> if  @channelId === @channel.id, do: "text-primary dark:text-zinc-200 dark:group-hover:text-white", else: ""
+          <> if  @channel.id != @channel.id, do: "text-primary dark:text-zinc-200 dark:group-hover:text-white", else: ""
 
       }>
         {@channel.name}
       </p>
        <%!-- <%= if @channel.name != "general" and role != MemberRole.GUEST do %> --%>
-      <%= if @channel.name != "general" and @role != "Guest" do %>
+      <%= if @channel.name != "general" and @role != :GUEST do %>
         <div class="ml-auto flex items-center gap-x-2">
           <%!-- <ActionTooltip label="Edit"> --%>
-          <button
+          <span
             phx-click="edit_click"
             phx-target={@myself}
             class="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
           >
             <.icon name="hero-pencil" class="w-4 h-4" />
-          </button>
+          </span>
            <%!-- </ActionTooltip> --%> <%!-- <ActionTooltip label="Delete"> --%>
-          <button
+          <span
             phx-click="delete_click"
             phx-target={@myself}
             class="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
           >
             <.icon name="hero-trash" class="w-4 h-4" />
-          </button>
+          </span>
            <%!-- </ActionTooltip> --%>
         </div>
       <% end %>
 
       <%= if @channel.name == "general" do %>
-        <button
+        <span
           phx-click="delete_click"
           phx-target={@myself}
           class="ml-auto w-4 h-4 text-zinc-500 dark:text-zinc-400"
         >
           <.icon name="hero-lock-closed" class="w-4 h-4" />
-        </button>
+        </span>
       <% end %>
     </button>
+
+
     """
   end
 
   @impl true
   def update(assigns, socket) do
+    IO.inspect(assigns)
     {:ok, socket |> assign(assigns)}
   end
 
@@ -69,6 +72,7 @@ defmodule DiscordCloneWeb.CustomComponents.Server.ServerChannel do
 
   @impl true
   def handle_event("delete_click", unsigned_params, socket) do
+    IO.inspect("delete_click")
     {:noreply, socket}
   end
 end

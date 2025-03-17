ad317530a0e4b86f5fcb2d931fa6956fc77d6a07
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-17 21:37:44 +0530

    chore: Added search_click handle event for server search modal popup

--- lib/discord_clone_web/live/custom_components/server/server_search.ex
+++ lib/discord_clone_web/live/custom_components/server/server_search.ex
@@ -5,23 +5,18 @@ defmodule DiscordCloneWeb.CustomComponents.Server.ServerSearch do
   def render(assigns) do
     ~H"""
     <button
-      phx-click="serch_click"
+      phx-click="search_click"
       phx-target={@myself}
       class="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
     >
       <.icon name="hero-magnifying-glass" class="w-4 h-4 text-zinc-500 dark:text-zinc-400" />
+      <p class="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
+        Search
+      </p>
 
-      <p
-          class="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition"
-        >
-          Search
-        </p>
-        <kbd
-          class="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto"
-        >
-          <span class="text-xs">cmd</span>K
-        </kbd>
-
+      <kbd class="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto">
+        <span class="text-xs">cmd</span>K
+      </kbd>
     </button>
      <%!-- Add the modal in the parant live_view --%>
     """
@@ -29,11 +24,20 @@ defmodule DiscordCloneWeb.CustomComponents.Server.ServerSearch do
 
   @impl true
   def update(assigns, socket) do
-    {:ok, socket|> assign(assigns)}
+
+    {:ok, socket |> assign(assigns)}
   end
 
   @impl true
-  def handle_event("serch_click", unsigned_params, socket) do
+  def handle_event("search_click", _unsigned_params, socket) do
+    search_obj = %{
+      label: "Search",
+      id: "search_server",
+      module: DiscordCloneWeb.CustomComponents.Search.SeverSearch
+    }
+
+    send(self(), {:open_modal, {search_obj, %{server_data: socket.assigns.server, server_search_data: socket.assigns.data}}})
+
     {:noreply, socket}
   end
 end

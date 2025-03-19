f03e36d5b3359c077c8cbb0daf953c473f480161
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-19 21:52:16 +0530

    refactor: template refactor

--- lib/discord_clone_web/live/custom_components/server/server_member.ex
+++ lib/discord_clone_web/live/custom_components/server/server_member.ex
@@ -4,34 +4,26 @@ defmodule DiscordCloneWeb.CustomComponents.Server.ServerMember do
   @impl true
   def render(assigns) do
     ~H"""
-    <.button
-      phx-click="onClick"
+    <div>
+     <.button
+      phx-click="on_member_click"
       phx-target={@myself}
       class={"group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1"
-      <> if   @memberId === @member.id, do: "bg-zinc-700/20 dark:bg-zinc-700", else: ""
+      <> if   @member_id == @member.id, do: "bg-zinc-700/20 dark:bg-zinc-700", else: ""
       }
     >
-      <.icon name="hero-user" class="h-8 w-8 md:h-8 md:w-8" />
+      <.icon name="hero-user" class="h-5 w-5 md:h-5 md:w-5" />
       <p class={   "font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition"
-        <> if   @memberId === @member.id, do: "text-primary dark:text-zinc-200 dark:group-hover:text-white", else: ""
+        <> if   @member_id == @member.id, do: "text-primary dark:text-zinc-200 dark:group-hover:text-white", else: ""
         }>
-        {@member.profile.name}
+        {@member.profile.user.name}
       </p>
        <.icon name={"hero-#{@icon[:name]}"} class={@icon[:class]} />
     </.button>
+    </div>
+
     """
   end
 
-  @impl true
-  def update(assigns, socket) do
-    role_icon_map = %{
-      :guest => nil,
-      :moderator => %{name: "shield_check", class: "h-4 w-4 ml-2 text-indigo-500"},
-      :admin => %{name: "shield_alert", class: "h-4 w-4 ml-2 text-rose-500"}
-    }
 
-    icon = Map.get(role_icon_map, assigns.member.role, nil)
-
-    {:ok, socket |> assign(assigns) |> assign(icon: icon)}
-  end
 end

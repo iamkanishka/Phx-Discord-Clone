12caae5d252359ddaf2745b74c464ed44f2ad4c6
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-19 21:55:03 +0530

    chore: Added circular wave effect and Discord logo to invite modal

--- lib/discord_clone_web/live/invite/invite.ex
+++ lib/discord_clone_web/live/invite/invite.ex
@@ -4,11 +4,23 @@ defmodule DiscordCloneWeb.Invite.Invite do
   @impl true
   def render(assigns) do
     ~H"""
-    <.modal   id="invite-modal" show on_cancel={JS.patch(~p"/")}>
-    <.live_component
-      module={DiscordCloneWeb.CustomComponents.Modals.InviteModal}
-      id={:invite_invite_modal}
-    />
+     <.modal id="invite-modal" show on_cancel={JS.patch(~p"/")}>
+      <div class="flex justify-center items-center w-full h-48">
+        <div class="relative">
+          <!-- Image -->
+          <img src="/images/discord_clone.png" alt="Profile" class="w-16 h-16 rounded-full" />
+
+    <!-- Circular Wave Effect -->
+          <%= if @invite_joining_status do %>
+            <div class="absolute inset-0 flex justify-center items-center">
+              <span class="absolute w-24 h-24 bg-blue-300 opacity-30 rounded-full animate-ping">
+              </span>
+              <span class="absolute w-16 h-16 bg-blue-400 opacity-50 rounded-full animate-ping delay-200">
+              </span>
+            </div>
+          <% end %>
+        </div>
+      </div>
     </.modal>
     """
   end

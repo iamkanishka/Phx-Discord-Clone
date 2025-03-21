253edd584ec978eee49c3c69f738bf7ff81d38a6
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-21 21:32:42 +0530

    feat: trigger event for create_server modal

--- lib/discord_clone_web/live/custom_components/navigation/navigation_action.ex
+++ lib/discord_clone_web/live/custom_components/navigation/navigation_action.ex
@@ -2,12 +2,11 @@ defmodule DiscordCloneWeb.CustomComponents.Navigation.NavigationAction do
   use DiscordCloneWeb, :live_component
 
   @impl true
-
   def render(assigns) do
     ~H"""
     <div>
       <.tooltip side="right" align="center" label="Add a server">
-        <button phx-click="create_server" class="group flex items-center">
+        <button phx-click="create_server" phx-target={@myself} class="group flex items-center">
           <div class="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500">
             <!-- Using Heroicons Plus icon as SVG instead of component -->
             <.icon name="hero-plus" class="group-hover:text-white transition text-emerald-500" />
@@ -23,10 +22,19 @@ defmodule DiscordCloneWeb.CustomComponents.Navigation.NavigationAction do
     {:ok, socket |> assign(assigns)}
   end
 
-  def handle_event("create_server", _params, socket) do
-    # Handle your create server logic here
-    # For this example, we'll just log it
-    IO.puts("Create server clicked!")
+
+
+  @impl true
+  def handle_event("create_server", _unsigned_params, socket) do
+    search_obj = %{
+      label: "Create Server",
+      id: "create_server",
+      module: DiscordCloneWeb.CustomComponents.Modals.CreateServerModal
+    }
+
+    send(self(), {:open_modal, {search_obj, %{}}})
+
     {:noreply, socket}
   end
+
 end

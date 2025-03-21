8bf6f2f5ce46eefdcfcf4c5e038f03f047fecf85
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-21 21:40:08 +0530

    chore: Added chat required comp

--- lib/discord_clone_web/live/servers/server.ex
+++ lib/discord_clone_web/live/servers/server.ex
@@ -25,6 +25,50 @@ defmodule DiscordCloneWeb.Servers.Server do
         user_image={@user_image}
         server_id={@server_id}
       >
+
+
+
+         <%!-- <div class="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader name={@channel.name} server_id={@channel.server_id} type="channel" />
+
+      <%= case @channel.type do %>
+        <% "text" -> %>
+          <.live_component module={ChatMessages}
+            id={"chat_#{@channel.id}"}
+            member={@member}
+            name={@channel.name}
+            chat_id={@channel.id}
+            type="channel"
+            api_url="/api/messages"
+            socket_url="/api/socket/messages"
+            socket_query={%{channel_id: @channel.id, server_id: @channel.server_id}}
+            param_key="channelId"
+            param_value={@channel.id}
+          />
+          <.live_component module={ChatInput}
+            name={@channel.name}
+            type="channel"
+            api_url="/api/socket/messages"
+            query={%{channel_id: @channel.id, server_id: @channel.server_id}}
+          />
+
+        <% "audio" -> %>
+          <.live_component module={MediaRoom}
+            chat_id={@channel.id}
+            video={false}
+            audio={true}
+          />
+
+        <% "video" -> %>
+          <.live_component module={MediaRoom}
+            chat_id={@channel.id}
+            video={true}
+            audio={true}
+          />
+      <% end %>
+      </div> --%>
+
+
       </.live_component>
     </div>
 

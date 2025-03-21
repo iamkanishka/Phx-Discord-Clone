a5dceba5fc3278ed68936c7b69267f83952b8fca
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-21 21:31:51 +0530

    feat: Added on_delete_confirm handle event with delete functionality

--- lib/discord_clone_web/live/custom_components/modals/delete_server_modal.ex
+++ lib/discord_clone_web/live/custom_components/modals/delete_server_modal.ex
@@ -1,4 +1,5 @@
 defmodule DiscordCloneWeb.CustomComponents.Modals.DeleteServerModal do
+  alias DiscordClone.Servers.Servers
   use DiscordCloneWeb, :live_component
 
   @impl true
@@ -15,19 +16,24 @@ defmodule DiscordCloneWeb.CustomComponents.Modals.DeleteServerModal do
         will be permanently deleted.
       </div>
 
-      <div class=" flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2 bg-gray-100 px-6 py-4">
+      <div class=" flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2  px-6 py-4">
         <div class="flex items-center justify-between w-full">
-          <.button disabled={@is_loading} phx-click="on_close" phx-target={@myself} class="ghost">
+          <.button
+            phx-click="on_close"
+            phx-target={@myself}
+            class="phx-submit-loading:opacity-75 rounded-lg bg-zinc-900 hover:bg-zinc-700 py-2 px-3 text-sm font-semibold leading-6 text-white active:text-white/80"
+          >
             Cancel
           </.button>
 
           <.button
-            disabled={@is_loading}
-            class="primary"
-            phc-click="on_delete_confirm"
+            phx-click="on_delete_confirm"
             phx-target={@myself}
+            class="phx-submit-loading:opacity-75 rounded-lg bg-zinc-900 hover:bg-zinc-700 py-2 px-3 text-sm font-semibold leading-6 text-white active:text-white/80"
+            phx-disable-with="Deleting..."
+
           >
-            Confirm
+            Delete
           </.button>
         </div>
       </div>
@@ -41,12 +47,47 @@ defmodule DiscordCloneWeb.CustomComponents.Modals.DeleteServerModal do
   end
 
   @impl true
-  def handle_event("on_close", unsigned_params, socket) do
+  def handle_event("on_close", _unsigned_params, socket) do
     {:noreply, socket}
   end
 
   @impl true
-  def handle_event("on_delete_confirm", unsigned_params, socket) do
+  def handle_event("on_delete_confirm", _unsigned_params, socket) do
+    socket = assign(socket, :is_loading, true)
+
+    socket =
+      with {:redirect, path} <-
+             Servers.delete_server_and_redirect(
+               socket.assigns.user_id,
+               socket.assigns.server.profile_id
+             ) do
+        socket =
+          if String.contains?(path, "/auth/sign-in") do
+            socket
+            |> redirect(to: path)
+          else
+            socket
+            |> push_navigate(to: path, replace: true)
+          end
+
+        socket
+      else
+        # {:ok, :no_server_found} ->
+        #   IO.puts("No server found, show server creation UI")
+        #   socket
+
+        # {:error, changeset} ->
+        #   IO.inspect(changeset, label: "Error creating profile")
+        #   socket
+        {:error, :unauthenticated} ->
+          {:redirect, "/auth/logout"}
+
+        # {:error, changeset} -> {:error, changeset}
+        {:error, error} ->
+          IO.inspect(error)
+          socket |> assign(:invite_joining_status, false)
+      end
+
     {:noreply, socket}
   end
 end

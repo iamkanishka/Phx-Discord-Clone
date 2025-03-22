5dcd617d3eb27d8f49f78a3182307f1d2295cee3
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-03-22 23:25:08 +0530

    VideoCallLive.Grant

---
+++ lib/discord_clone_web/live/video_call_live/grant.ex
@@ -0,0 +1,27 @@
+defprotocol DiscordCloneWeb.VideoCallLive.Grant do
+  @moduledoc """
+  A protocol for converting grants into JWT claims.
+  """
+
+  @doc """
+  The type of claim this grant is.
+
+  ## Examples
+
+      def type(_grant), do: "chat"
+
+  """
+  def type(grant)
+
+  @doc """
+  The attributes of the claim.
+
+  ## Examples
+
+      def attrs(grant) do
+        %{"name" => grant.name}
+      end
+
+  """
+  def attrs(grant)
+end

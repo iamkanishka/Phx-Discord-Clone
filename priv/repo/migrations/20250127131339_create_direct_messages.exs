c54e22b45e11dee9998a08d096e815ae4cb39f81
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-04-05 21:34:43 +0530

    chore: Removed content as required and added file_type

--- priv/repo/migrations/20250127131339_create_direct_messages.exs
+++ priv/repo/migrations/20250127131339_create_direct_messages.exs
@@ -4,12 +4,12 @@ defmodule DiscordClone.Repo.Migrations.CreateDirectMessages do
   def change do
     create table(:direct_messages, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :content, :text, null: false
+      add :content, :text
       add :file_url, :text
+      add :file_type, :text
       add :deleted, :boolean, default: false
 
       timestamps()
     end
-
   end
 end
